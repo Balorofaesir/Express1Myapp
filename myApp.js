f107759bd12df48fa2ada08b4ee6593bcd1a1530
Baldorofaesir <salomoneyd@gmail.com>
2022-10-27 15:42:17 -0500

    First v Of my Epress Ex

---
+++ myApp.js
@@ -0,0 +1,108 @@
+const express = require("express");
+
+const app = express();
+
+const about = {
+    name: "Salomón Vásquez",
+    age: 29,
+    email: "ejemplo@ejemplo.com",
+    phone: "12345",
+    adress: "123 Main st",
+    city: "Medellín",
+    headline: "student",
+    links : [
+        {
+            name: "1",
+            url: "https://ejemplo1",
+        },
+        {
+            name: "2",
+            url: "https://ejemplo2",
+        }
+    ],
+    website: "https://IdontHaveAwebSite.com",
+    skills: [
+        {
+            name: "Javascript",
+            level: "Rookie"
+        },
+        {
+            name: "React",
+            level: "Rookie",
+        },
+        {
+            name: "Node",
+            level: "Rookie"
+        },
+        {
+            name: "HTML",
+            level: "Rookie"
+        },     
+    ]
+}
+
+const products = [
+    {
+    id: 1,
+    title: "new Title marcos",
+    price: 100004,
+    description: "new Description",
+    images: [
+      "https://api.lorem.space/image/shoes?w=640&h=480&r=5193",
+      "https://api.lorem.space/image/shoes?w=640&h=480&r=2221",
+      "https://api.lorem.space/image/shoes?w=640&h=480&r=2672",
+    ],
+  },
+  {
+    id: 3,
+    title: "new try",
+    price: 29,
+    description:
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    images: [
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=4308",
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=4555",
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=4493",
+    ],
+  },
+  {
+    id: 4,
+    title: "Licensed Plastic Table",
+    price: 808,
+    description:
+      "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+    images: [
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=6769",
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=6810",
+      "https://api.lorem.space/image/fashion?w=640&h=480&r=2350",
+    ],
+  },
+  {
+    id: 5,
+    title: "Refined Plastic Shirt",
+    price: 252,
+    description:
+      "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    images: [
+      "https://api.lorem.space/image/watch?w=640&h=480&r=9592",
+      "https://api.lorem.space/image/watch?w=640&h=480&r=6436",
+      "https://api.lorem.space/image/watch?w=640&h=480&r=4565",
+    ],
+  }
+] 
+
+console.log("MyFirstExpress");
+
+app.get("/info", function (req, res) {
+  res.send(`<p>Our store has ${products.length} products</p>
+  <p>${new Date()}</p>`);
+});
+app.get("/api/products", function (req, res) {
+  res.json(products);
+});
+
+app.get("/about", function (req, res) {
+    res.json(about);
+  });
+
+app.listen(8080);
